6ecdbff823991af8ffc5adfe29c2f20c6b29e085
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-09-13 22:31:44 +0300

    add deployment to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/car-rental-app",
+  base: "/car-rental-app/",
   plugins: [react()],
 });
