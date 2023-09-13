c3de8934aa9ed30bef7096fb3df9e9262276e941
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-09-13 23:26:00 +0300

    attempt 3 router on gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,22 +6,27 @@ import App from "./App";
 import Catalog from "./pages/Catalog/Catalog";
 import Favorites from "./pages/Favorites/Favorites";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/catalog",
-        element: <Catalog />,
-      },
-      {
-        path: "/favorites",
-        element: <Favorites />,
-      },
-    ],
-  },
-]);
+const baseName = "/car-rental-app";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/catalog",
+          element: <Catalog />,
+        },
+        {
+          path: "/favorites",
+          element: <Favorites />,
+        },
+      ],
+    },
+  ],
+  { basename: baseName }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
