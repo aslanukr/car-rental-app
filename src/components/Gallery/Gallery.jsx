3eecdf46299c315533c926248a4c510b8ac1138e
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-09-17 15:27:55 +0300

    change pagination to local state

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,29 +3,47 @@ import { GalleryGrid, GalleryWrapper, LoadMoreBtn } from "./Gallery.styled";
 
 import CarCard from "../CarCard/CarCard";
 
-import { useDispatch, useSelector } from "react-redux";
-import {
-  selectCars,
-  selectCarsError,
-  selectCarsIsLoading,
-  selectFavorites,
-} from "src/redux/selectors";
-import { getCarsThunk } from "src/redux/cars/carsThunk";
+import { useSelector } from "react-redux";
+import { selectFavorites } from "src/redux/selectors";
+
 import Loader from "../Loader/Loader";
 import Error from "../Error/Error";
+import { getCatalog } from "src/services/api";
 
 const Gallery = ({ renderFavorites }) => {
-  const cars = useSelector(selectCars);
+  // const cars = useSelector(selectCars);
   const favorites = useSelector(selectFavorites);
-  const isLoading = useSelector(selectCarsIsLoading);
-  const error = useSelector(selectCarsError);
+  // const isLoading = useSelector(selectCarsIsLoading);
+  // const error = useSelector(selectCarsError);
+  const [cars, setCars] = useState([]);
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(getCarsThunk(page));
+  // }, [dispatch, page]);
+
+  const fetchCars = async (page) => {
+    setIsLoading(true);
+    try {
+      const response = await getCatalog(page);
+      const newCars = response;
+      setCars((prevCars) => [...prevCars, ...newCars]);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    dispatch(getCarsThunk(page));
-  }, [dispatch, page]);
+    if (page === 1) {
+      fetchCars(page);
+    }
+  }, [page]);
 
   const favoriteCars = cars.filter((car) => favorites.includes(car.id));
 
